453fda949527fb72cf0189dcb4da1f7749c2e2ca
PedroCamillozzi <pedro_camillozzi@hotmail.com>
2024-06-06 20:16:06 -0300

    TP 2.1

---
+++ 3-Generadores PseudoAleatorios/Distribuciones-de-Probabilidad.py
@@ -0,0 +1,382 @@
+import random as rnd
+import matplotlib.pyplot as plt
+import math
+
+
+class GLC:
+    def __init__(self, seed, a, c, m):
+        self.state = seed
+        self.a = a
+        self.c = c
+        self.m = m
+#       seed=> semilla inicial, a=> multiplicador, c=> incremento constante, m=> módulo
+
+    def random(self):
+        self.state = (self.a * self.state + self.c) % self.m
+        return self.state / self.m
+
+
+class MersenneTwister:
+    def __init__(self, seed):
+        self.mt = [0] * 624
+        self.index = 0
+        self.mt[0] = seed
+        for i in range(1, 624):
+            self.mt[i] = (0x6c078965 * (self.mt[i - 1] ^ (self.mt[i - 1] >> 30)) + i) & 0xffffffff
+
+    def extract_number(self):
+        if self.index == 0:
+            self._twist()
+
+        y = self.mt[self.index]
+        y = y ^ (y >> 11)
+        y = y ^ ((y << 7) & 0x9d2c5680)
+        y = y ^ ((y << 15) & 0xefc60000)
+        y = y ^ (y >> 18)
+
+        self.index = (self.index + 1) % 624
+        return y / 0xffffffff
+
+    def _twist(self):
+        for i in range(624):
+            y = (self.mt[i] & 0x80000000) + (self.mt[(i + 1) % 624] & 0x7fffffff)
+            self.mt[i] = self.mt[(i + 397) % 624] ^ (y >> 1)
+            if y % 2 != 0:
+                self.mt[i] = self.mt[i] ^ 0x9908b0df
+
+
+def exponential_transform(u, lambd):
+    return -math.log(1 - u) / lambd
+
+
+def gamma_transform(u_list, beta):
+    return sum(-math.log(1 - u) / beta for u in u_list)
+
+
+def normal_transform(u1, u2):
+    z0 = math.sqrt(-2.0 * math.log(u1)) * math.cos(2.0 * math.pi * u2)
+    z1 = math.sqrt(-2.0 * math.log(u1)) * math.sin(2.0 * math.pi * u2)
+    return z0, z1
+
+
+def pascal_transform(u_list, r, p):
+    n = 0
+    successes = 0
+    while successes < r:
+        if not u_list:
+            raise ValueError("Lista de números aleatorios agotada")
+        if u_list.pop(0) < p:
+            successes += 1
+        n += 1
+    return n
+
+
+def binomial_transform(u_list, n, p):
+    successes = 0
+    for _ in range(n):
+        if not u_list:
+            raise ValueError("Lista de números aleatorios agotada")
+        if u_list.pop(0) < p:
+            successes += 1
+    return successes
+
+
+def hypergeometric_transform(u_list, N, K, n):
+    good_elements = 0
+    for _ in range(n):
+        if not u_list:
+            raise ValueError("Lista de números aleatorios agotada")
+        if u_list.pop(0) < K / N:
+            good_elements += 1
+            K -= 1
+        N -= 1
+    return good_elements
+
+
+def poisson_transform(u_list, lambd):
+    n = 0
+    p = math.exp(-lambd)
+    F = p
+    while True:
+        if not u_list:
+            raise ValueError("Lista de números aleatorios agotada")
+        if u_list.pop(0) < F:
+            return n
+        n += 1
+        p *= lambd / n
+        F += p
+
+
+def empirical_discrete_transform(u_list, probabilities):
+    cumulative_probabilities = [sum(probabilities[:i+1]) for i in range(len(probabilities))]
+    for i, cumulative_prob in enumerate(cumulative_probabilities):
+        if not u_list:
+            raise ValueError("Lista de números aleatorios agotada")
+        if u_list[0] < cumulative_prob:
+            return i
+    raise ValueError("No se pudo generar un valor con la distribución de probabilidad empírica discreta")
+
+
+def main():
+    glcresults = []
+    mtresults = []
+    randomlibraryresults = []
+
+    lambd = 1
+    alpha = 2
+    beta = 1
+
+    seed = 42
+    a = 1664525
+    c = 1013904223
+    m = 2 ** 32
+
+
+
+    op = int(input("Seleccione el tipo de Distribución: 1-Uniforme 2-Exponencial 3-Gamma 4-Normal 5-Pascal 6-Binomial 7-Hipergeométrica 8- Poisson 9-Empírica Discreta: "))
+
+    generatorglc = GLC(seed, a, c, m)
+
+    print('GLC')
+    resultaux = None
+    for i in range(5000):
+        result = generatorglc.random()
+        if op == 1:
+            glcresults.append(result)
+            print(glcresults[i])
+        elif op == 2:
+            glcresults.append(exponential_transform(result, lambd))
+            print(glcresults[i])
+        elif op == 3:
+            listaux = [generatorglc.random() for _ in range(alpha)]
+            glcresults.append(gamma_transform(listaux, beta))
+            print(glcresults[i])
+        elif op == 4:
+            if resultaux is None:
+                resultaux = result
+            else:
+                z0, z1 = normal_transform(resultaux, result)
+                glcresults.extend([z0, z1])
+                resultaux = None
+                print(glcresults[i-1])
+                print(glcresults[i])
+        elif op == 5:
+            p = 0.5
+            r = 5
+            listaux = [generatorglc.random() for _ in range(10)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                pascal_result = pascal_transform(listaux, r, p)
+                glcresults.append(pascal_result)
+                print(glcresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 6:
+            n = 10
+            p = 0.5
+            listaux = [generatorglc.random() for _ in range(n)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                binomial_result = binomial_transform(listaux, n, p)
+                glcresults.append(binomial_result)
+                print(glcresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 7:
+            N = 100  # Tamaño de la población
+            K = 20  # Número de elementos en la población con la característica de interés
+            n = 10  # Tamaño de la muestra
+            listaux = [generatorglc.random() for _ in range(N)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                hypergeometric_result = hypergeometric_transform(listaux, N, K, n)
+                glcresults.append(hypergeometric_result)
+                print(glcresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 8:
+            listaux = [generatorglc.random() for _ in
+                       range(10)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                poisson_result = poisson_transform(listaux, lambd)
+                glcresults.append(poisson_result)
+                print(glcresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 9:
+            probabilities = [0.2, 0.3, 0.1, 0.25, 0.15]
+            listaux = [generatorglc.random() for _ in range(len(probabilities))]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                empirical_discrete_result = empirical_discrete_transform(listaux, probabilities)
+                glcresults.append(empirical_discrete_result)
+                print(glcresults[-1])
+            except ValueError as e:
+                print("")
+
+    print('random library, seed setted = ', seed)
+    rnd.seed(seed)
+    resultaux = None
+    for i in range(5000):
+        result = rnd.random()
+        if op == 1:
+            randomlibraryresults.append(result)
+            print(randomlibraryresults[i])
+        elif op == 2:
+            randomlibraryresults.append(exponential_transform(result, lambd))
+            print(randomlibraryresults[i])
+        elif op == 3:
+            listaux = [rnd.random() for _ in range(alpha)]
+            randomlibraryresults.append(gamma_transform(listaux, beta))
+            print(randomlibraryresults[i])
+        elif op == 4:
+            if resultaux is None:
+                resultaux = result
+            else:
+                z0, z1 = normal_transform(resultaux, result)
+                randomlibraryresults.extend([z0, z1])
+                resultaux = None
+                print(randomlibraryresults[i-1])
+                print(randomlibraryresults[i])
+        elif op == 5:
+            p = 0.5
+            r = 5
+            listaux = [rnd.random() for _ in range(10)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                pascal_result = pascal_transform(listaux, r, p)
+                randomlibraryresults.append(pascal_result)
+                print(randomlibraryresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 6:
+            n = 10
+            p = 0.5
+            listaux = [rnd.random() for _ in range(n)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                binomial_result = binomial_transform(listaux, n, p)
+                randomlibraryresults.append(binomial_result)
+                print(randomlibraryresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 7:
+            N = 100  # Tamaño de la población
+            K = 20  # Número de elementos en la población con la característica de interés
+            n = 10  # Tamaño de la muestra
+            listaux = [rnd.random() for _ in range(N)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                hypergeometric_result = hypergeometric_transform(listaux, N, K, n)
+                randomlibraryresults.append(hypergeometric_result)
+                print(randomlibraryresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 8:
+            listaux = [rnd.random() for _ in
+                       range(10)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                poisson_result = poisson_transform(listaux, lambd)
+                randomlibraryresults.append(poisson_result)
+                print(randomlibraryresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 9:
+            probabilities = [0.2, 0.3, 0.1, 0.25, 0.15]
+            listaux = [rnd.random() for _ in range(len(probabilities))]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                empirical_discrete_result = empirical_discrete_transform(listaux, probabilities)
+                randomlibraryresults.append(empirical_discrete_result)
+                print(randomlibraryresults[-1])
+            except ValueError as e:
+                print("")
+
+
+    print('Mersenne Twister, seed setted = ', seed)
+    generatormt = MersenneTwister(seed)
+    resultaux = None
+    for i in range(5000):
+        result = generatormt.extract_number()
+        if op == 1:
+            mtresults.append(result)
+            print(mtresults[i])
+        elif op == 2:
+            mtresults.append(exponential_transform(result, lambd))
+            print(mtresults[i])
+        elif op == 3:
+            listaux = [generatormt.extract_number() for _ in range(alpha)]
+            mtresults.append(gamma_transform(listaux, beta))
+            print(mtresults[i])
+        elif op == 4:
+            if resultaux is None:
+                resultaux = result
+            else:
+                z0, z1 = normal_transform(resultaux, result)
+                mtresults.extend([z0, z1])
+                resultaux = None
+                print(mtresults[i-1])
+                print(mtresults[i])
+        elif op == 5:
+            p = 0.5
+            r = 5
+            listaux = [generatormt.extract_number() for _ in range(10)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                pascal_result = pascal_transform(listaux, r, p)
+                mtresults.append(pascal_result)
+                print(mtresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 6:
+            n = 10
+            p = 0.5
+            listaux = [generatormt.extract_number()  for _ in range(n)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                binomial_result = binomial_transform(listaux, n, p)
+                mtresults.append(binomial_result)
+                print(mtresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 7:
+            N = 100  # Tamaño de la población
+            K = 20  # Número de elementos en la población con la característica de interés
+            n = 10  # Tamaño de la muestra
+            listaux = [generatormt.extract_number() for _ in range(N)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                hypergeometric_result = hypergeometric_transform(listaux, N, K, n)
+                mtresults.append(hypergeometric_result)
+                print(mtresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 8:
+            listaux = [generatormt.extract_number() for _ in
+                       range(10)]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                poisson_result = poisson_transform(listaux, lambd)
+                mtresults.append(poisson_result)
+                print(mtresults[-1])
+            except ValueError as e:
+                print("")
+        elif op == 9:
+            probabilities = [0.2, 0.3, 0.1, 0.25, 0.15]
+            listaux = [generatormt.extract_number() for _ in range(len(probabilities))]  # Genera una lista de números aleatorios uniformemente distribuidos
+            try:
+                empirical_discrete_result = empirical_discrete_transform(listaux, probabilities)
+                mtresults.append(empirical_discrete_result)
+                print(mtresults[-1])
+            except ValueError as e:
+                print("")
+
+    plt.figure(figsize=(15, 5))
+
+    plt.subplot(1, 3, 1)
+    plt.hist(glcresults, bins=50, color='blue', alpha=0.7)
+    plt.title('GLC')
+
+    plt.subplot(1, 3, 2)
+    plt.hist(mtresults, bins=50, color='green', alpha=0.7)
+    plt.title('Mersenne Twister')
+
+    plt.subplot(1, 3, 3)
+    plt.hist(randomlibraryresults, bins=50, color='red', alpha=0.7)
+    plt.title('Random Library')
+
+    plt.suptitle('Distribución Empírica Discreta de Números Pseudoaleatorios')
+    plt.savefig('Distribución de Probabilidad Empírica Discreta')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- 3-Generadores PseudoAleatorios/Generador-PseudoAletorio.py
+++ 3-Generadores PseudoAleatorios/Generador-PseudoAletorio.py
@@ -2,6 +2,7 @@ import random as rnd
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LogNorm
+from scipy.stats import chi2
 
 
 class GLC:
@@ -17,34 +18,60 @@ class GLC:
         return self.state / self.m
 
 
-class MidSquareRNG:
-    def __init__(self, seed, n_digits):
-        self.seed = seed
-        self.n_digits = n_digits
+class MersenneTwister:
+    def __init__(self, seed):
+        self.mt = [0] * 624
+        self.index = 0
+        self.mt[0] = seed
+        for i in range(1, 624):
+            self.mt[i] = (0x6c078965 * (self.mt[i - 1] ^ (self.mt[i - 1] >> 30)) + i) & 0xffffffff
 
-    def random(self):
-        # Elevar al cuadrado el número semilla
-        squared = self.seed ** 2
+    def extract_number(self):
+        if self.index == 0:
+            self._twist()
 
-        # Convertir a string para facilitar la extracción de los dígitos centrales
-        squared_str = str(squared).zfill(2 * self.n_digits)
+        y = self.mt[self.index]
+        y = y ^ (y >> 11)
+        y = y ^ ((y << 7) & 0x9d2c5680)
+        y = y ^ ((y << 15) & 0xefc60000)
+        y = y ^ (y >> 18)
 
-        # Calcular el número de dígitos a eliminar en cada lado
-        start = (len(squared_str) - self.n_digits) // 2
-        end = start + self.n_digits
+        self.index = (self.index + 1) % 624
+        return y / 0xffffffff
 
-        # Extraer los dígitos centrales
-        new_seed_str = squared_str[start:end]
+    def _twist(self):
+        for i in range(624):
+            y = (self.mt[i] & 0x80000000) + (self.mt[(i + 1) % 624] & 0x7fffffff)
+            self.mt[i] = self.mt[(i + 397) % 624] ^ (y >> 1)
+            if y % 2 != 0:
+                self.mt[i] = self.mt[i] ^ 0x9908b0df
 
-        # Convertir los dígitos centrales de vuelta a un número entero
-        self.seed = int(new_seed_str)
+def chi_square_test(observed, expected):
+    chi_squared = sum((o - e) ** 2 / e for o, e in zip(observed, expected))
+    degrees_of_freedom = len(observed) - 1
+    p_value = 1 - chi2.cdf(chi_squared, degrees_of_freedom)
+    return chi_squared, p_value
 
-        return self.seed
+
+def waiting_time_test(sequence, subinterval):
+    count = sum(1 for num in sequence if subinterval[0] <= num < subinterval[1])
+    prob_subinterval = count / len(sequence)
+    waiting_times = []
+    last_index = -1
+    for i, num in enumerate(sequence):
+        if subinterval[0] <= num < subinterval[1]:
+            if last_index != -1:
+                waiting_times.append(i - last_index - 1)
+            last_index = i
+    p = 1 - prob_subinterval
+    expected_waiting_times = [1 / p] * len(waiting_times)
+    chi_squared, p_value = chi_square_test(waiting_times, expected_waiting_times)
+    return chi_squared, p_value
 
 
 def main():
     glcresults = []
-    msmresults = []
+    mtresults = []
     randomlibraryresults = []
     seed = 42
     a = 1664525
@@ -64,33 +91,92 @@ def main():
         randomlibraryresults.append(rnd.random())
         print(randomlibraryresults[i])
 
-    print('Mid-Square')
-    seed = 1382
-    generatormsm = MidSquareRNG(seed, n_digits=4)
+    print('Mersenne Twister, seed setted = ', seed)
+    generatormt = MersenneTwister(seed)
     for i in range(5000):
-        nrorandom = generatormsm.random()
-        msmresults.append(nrorandom**2/100000000)
-        print('Semilla: ', nrorandom, 'Valor: ', msmresults[i])
-        if msmresults[i] == 0:
-            break
+        mtresults.append(generatormt.extract_number())
+        print(mtresults[i])
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-
-    x1 = randomlibraryresults
+    op = int(input("1- GLC vs MT 2- GLC vs Random Lirary 3- MT vs Random Lirary: "))
     y1 = np.linspace(0, 1, len(randomlibraryresults))
-    h1 = ax1.hist2d(x1, y1, bins=40, norm=LogNorm())
-    fig.colorbar(h1[3], ax=ax1)
-    ax1.set_title('Histograma Random Library')
+    y2 = np.linspace(0, 1, len(mtresults))
 
-    x2 = msmresults
-    y2 = np.linspace(0, 1, len(msmresults))
-    h2 = ax2.hist2d(x2, y2, bins=40, norm=LogNorm())
-    fig.colorbar(h2[3], ax=ax2)
-    ax2.set_title('Histograma Mid-Square')
+    if op == 1:
+        x1 = glcresults
+        x2 = mtresults
+        h1 = ax1.hist2d(x1, y1, bins=40, norm=LogNorm())
+        fig.colorbar(h1[3], ax=ax1)
+        h2 = ax2.hist2d(x2, y2, bins=40, norm=LogNorm())
+        ax1.set_title('Histograma GLC')
+        fig.colorbar(h2[3], ax=ax2)
+        ax2.set_title('Histograma Mesrsenne Twister')
+    elif op == 2:
+        x1 = glcresults
+        x2 = randomlibraryresults
+        h1 = ax1.hist2d(x1, y1, bins=40, norm=LogNorm())
+        fig.colorbar(h1[3], ax=ax1)
+        h2 = ax2.hist2d(x2, y2, bins=40, norm=LogNorm())
+        ax1.set_title('Histograma GLC')
+        fig.colorbar(h2[3], ax=ax2)
+        ax2.set_title('Histograma Random Lirary')
+    elif op == 3:
+        x1 = mtresults
+        x2 = randomlibraryresults
+        h1 = ax1.hist2d(x1, y1, bins=40, norm=LogNorm())
+        fig.colorbar(h1[3], ax=ax1)
+        h2 = ax2.hist2d(x2, y2, bins=40, norm=LogNorm())
+        ax2.set_title('Histograma Mersenne Twister')
+        fig.colorbar(h2[3], ax=ax2)
+        ax2.set_title('Histograma Random Lirary')
 
     plt.tight_layout()
-    plt.savefig('RandomLibraryvsMid-Square.png')
+#    plt.savefig('GLCvsMT.png')
     plt.show()
+
+    # Pruebas estadísticas
+    print("\nPruebas estadísticas GLC:")
+    # Prueba de bondad de ajuste (Chi-cuadrado)
+    print("\nPrueba de bondad de ajuste (Chi-cuadrado):")
+    expected_freq = [len(glcresults) / 10] * 10  # 10 bins
+    observed_freq, _ = np.histogram(glcresults, bins=10)
+    chi_squared, p_value = chi_square_test(observed_freq, expected_freq)
+    print("Chi-cuadrado:", chi_squared)
+    print("P-valor:", p_value)
+    # Prueba de espera
+    print("\nPrueba de espera:")
+    chi_squared, p_value = waiting_time_test(glcresults, (0.4, 0.6))
+    print("Chi-cuadrado:", chi_squared)
+    print("P-valor:", p_value)
+
+    print("\nPruebas estadísticas RANDOM LIBRARY:")
+    # Prueba de bondad de ajuste (Chi-cuadrado)
+    print("\nPrueba de bondad de ajuste (Chi-cuadrado):")
+    expected_freq = [len(randomlibraryresults) / 10] * 10  # 10 bins
+    observed_freq, _ = np.histogram(randomlibraryresults, bins=10)
+    chi_squared, p_value = chi_square_test(observed_freq, expected_freq)
+    print("Chi-cuadrado:", chi_squared)
+    print("P-valor:", p_value)
+    # Prueba de espera
+    print("\nPrueba de espera:")
+    chi_squared, p_value = waiting_time_test(randomlibraryresults, (0.4, 0.6))
+    print("Chi-cuadrado:", chi_squared)
+    print("P-valor:", p_value)
+
+    print("\nPruebas estadísticas Mersenne Twister:")
+    # Prueba de bondad de ajuste (Chi-cuadrado)
+    print("\nPrueba de bondad de ajuste (Chi-cuadrado):")
+    expected_freq = [len(mtresults) / 10] * 10  # 10 bins
+    observed_freq, _ = np.histogram(mtresults, bins=10)
+    chi_squared, p_value = chi_square_test(observed_freq, expected_freq)
+    print("Chi-cuadrado:", chi_squared)
+    print("P-valor:", p_value)
+    print("\nPruebas estadísticas Mersenne Twister:")
+    # Prueba de espera
+    print("\nPrueba de espera:")
+    chi_squared, p_value = waiting_time_test(mtresults, (0.4, 0.6))
+    print("Chi-cuadrado:", chi_squared)
+    print("P-valor:", p_value)
 
 
 if __name__ == "__main__":
